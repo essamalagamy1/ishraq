234c03182b652104ab09a83142bcdd7cb20b0c74
Essam Hamdy AL-agamy <20912018100106@fci.zu.edu.eg>
2026-02-17 01:23:26 +0200

    chore: move meta charset tag to a new position.

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -15,9 +15,10 @@
     </script>
     @endif
 
-    <meta charset="utf-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
 
     <!-- Preconnect to CDN origins -->
     <link rel="preconnect" href="https://cdn.jsdelivr.net" crossorigin>
